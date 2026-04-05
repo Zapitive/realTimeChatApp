3604fc1d77ca90057848dc8252a342db9290da0a
Zapitive <92020324+Zapitive@users.noreply.github.com>
2026-04-05 18:01:29 +0530

    misc-- socket cors configuration

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -10,7 +10,7 @@ const initSocket = (server) =>{
 
     io = new Server(server,{
         cors:{
-            origin: 'http://localhost:5173',
+            origin: process.env.FRONTEND_URL,
             methods: ['GET', 'POST', 'DELETE']
         }
     });
