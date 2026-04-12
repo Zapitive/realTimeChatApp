152d1844b57d3c21f9631f8dd975aff468e4141c
Zapitive <92020324+Zapitive@users.noreply.github.com>
2026-04-12 09:54:22 +0530

    Added logic for chatMessages status on getMessages for a chat, by default is sent and then check for seen or received, and only return if msg isOwn

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,3 +1,4 @@
+const chatRoom = require("../models/chatRoomModel");
 const message = require("../models/messageModel");
 
 
@@ -6,12 +7,11 @@ const getMessages = async (req, res) =>{
         const chatId = req.query?.chatId || "";
         const userId = req.user.id;
 
-        const messages = await message.find({chatId: chatId},{
-            senderId: 1,
-            content: 1,
-            createdAt : 1
-        }).limit(20);
-
+        const messages = await message.find({chatId: chatId}).limit(20);
+        const chat = await chatRoom.findById({_id: chatId},{
+            _id: 0,
+            members: 1
+        });
 
         const chatMessages = messages.map((msg) =>{
             
@@ -19,13 +19,23 @@ const getMessages = async (req, res) =>{
             const senderId = String(msg.senderId);
             const isOwn = userId === senderId;
             const date = new Date(msg.createdAt);
-
+            let msgStatus = 'sent';
+            if (isOwn){
+                if (chat.members.length - 1 === msg.seenBy.length){
+                    msgStatus = 'seen'
+                }
+                else if((chat.members.length - 1 === msg.receivedBy.length)){
+                    msgStatus = 'received'
+                }
+            }
+            
             const text = msg.content;
             
             return {
             id: id,
             text: text,
             isOwn: isOwn,
+            ...(isOwn && { msgStatus: msgStatus }),
             timestamp: date
             }
         });
