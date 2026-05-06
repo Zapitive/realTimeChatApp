7b271de81f3cf54d9a8ac49877f0899e7f355c3e
Zapitive <92020324+Zapitive@users.noreply.github.com>
2026-05-06 21:16:58 +0530

    refactored - usercontroller and userservice for business logic

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,58 +1,23 @@
-const { default: mongoose } = require('mongoose');
-const userInfo = require('../models/userInfoModel');
-const chatRoom = require('../models/chatRoomModel');
+const userService = require('../services/userService')
 
-const allUsers = async(req,res) =>{
-    // take all users except the current user
-    const users = await userInfo.find({_id:{$ne:req.user.id}}, {username:1}).limit(10);
+// const allUsers = async(req,res) =>{
+//     // take all users except the current user
+//     const users = await userInfo.find({_id:{$ne:req.user.id}}, {username:1}).limit(10);
     
-    const filteredUsers = users.map((user) =>({
-        ...user.toObject(),
-        status:"offline"
-    }));
-    return res.status(200).json({message:"Users found", users:filteredUsers})
-}
+//     const filteredUsers = users.map((user) =>({
+//         ...user.toObject(),
+//         status:"offline"
+//     }));
+//     return res.status(200).json({message:"Users found", users:filteredUsers})
+// }
 
 const searchUser = async(req,res) =>{
     try{
         const searchValue = req.query?.searchValue || "";
-        const regexValue = "^"+searchValue;
-        const userId = new mongoose.Types.ObjectId(req.user.id);
-
-        const chats = await chatRoom.find({
-            members: userId
-        },{
-            _id : 0,
-            members: 1
-        });
-
-        const contactedUsers = [
-            ...new Set(
-                chats.flatMap(c => c.members)
-            )
-        ].filter(id => id.toString() !== userId.toString());
-
-        const users = await userInfo.find({
-            $or:[
-                {username:{ $regex: regexValue, $options: "i" }},
-                {email:{ $regex: regexValue, $options: "i" }}
-            ],
-            _id:{
-                $ne: userId,
-                $nin: contactedUsers
-            }
-            },{
-                username:1,
-                status: 1
-        }).limit(10);
-
-        const filteredUsers = users.map((user)=>({
-            id : user._id.toString(),
-            name : user.username,
-            status: user.status
-        }));
-
-        return res.status(200).json({message:"Users found",users:filteredUsers});
+        const uId = req.user.id;
+        const result = userService.searchUser(searchValue, uId)
+        
+        return res.status(200).json({message:"Users found",users: result.filteredUsers});
 
     }catch(err){
         console.log(err)
--- server/services/userService.js
+++ server/services/userService.js
@@ -1,5 +1,6 @@
-const userInfo = require("../models/userInfoModel");
-
+const { default: mongoose } = require('mongoose');
+const userInfo = require('../models/userInfoModel');
+const chatRoom = require('../models/chatRoomModel');
 
 const setUserOnline = async (userId) =>{
     return await userInfo.findByIdAndUpdate(userId,{
@@ -18,4 +19,44 @@ const findUser = async (filter) =>{
     return await userInfo.findOne(filter).lean();
 }
 
-module.exports = {setUserOnline, setUserOffline, findUser}
+const searchUser = async ({searchValue, uId}) =>{
+    const regexValue = "^"+searchValue;
+    const userId = new mongoose.Types.ObjectId(uId);
+
+    const chats = await chatRoom.find({ members: userId},
+        {_id : 0,members: 1}).lean();
+
+    const contactedUsers = [
+        ...new Set(
+            chats.flatMap(c => c.members)
+        )
+    ].filter(id => id.toString() !== userId.toString());
+
+    const users = await userInfo.find({
+        $or:[
+            {username:{ $regex: regexValue, $options: "i" }},
+            {email:{ $regex: regexValue, $options: "i" }}
+        ],
+        _id:{
+            $ne: userId,
+            $nin: contactedUsers
+        }
+        },{
+            username:1,
+            status: 1
+    }).limit(10).lean();
+
+    if(users){
+        const filteredUsers = users.map((user)=>({
+            id : user._id.toString(),
+            name : user.username,
+            status: user.status
+        }));
+        return {filteredUsers: filteredUsers}
+    }
+    
+    return {filteredUsers: []}
+    
+}
+
+module.exports = {setUserOnline, setUserOffline, findUser, searchUser}
